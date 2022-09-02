e08faabc6672cae25cf98a3684833939060e2ac3
GuyOnLeft <jmunson0711@gmail.com>
2022-09-02 09:44:51 -0300

    repeat string esercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(inputWord, numChoice) {  
+    if (numChoice >= 0)
+        return inputWord.repeat(numChoice);
+    else if (numChoice < 0) {
+        return "ERROR";
+    }
+    else   
+        return " ";
 
-};
+}
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
